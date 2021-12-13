529781e9fc7d65b9271b31dbf1094bff34a4c919
Sebastian Balling <coolchip@gmx.de>
2021-12-13 14:26:36 +0100

    update package dependencies and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'env': {
+        'node': true,
+        'commonjs': true,
+        'es2021': true,
+        'mocha': true,
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 2021
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
